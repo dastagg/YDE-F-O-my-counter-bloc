0f6b2d053732f57c2d3aa8362bef1fa97cc4af80
dastagg:opus5 <dastagg@stagglinux.com>
2022-04-08 18:31:55 -0400

    Implement the Bloc modules.

---
+++ lib/blocs/counter/counter_state.dart
@@ -0,0 +1,34 @@
+part of 'counter_bloc.dart';
+
+// Steps done in this file:
+
+// Deleted the original abstract class and replaced with
+// current class
+// added final int counter
+// used generator to create Constructor, equatable, toString and copyWith.
+// created factory contructor to return the instance.
+
+class CounterState extends Equatable {
+  final int counter;
+  CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
+
+  @override
+  List<Object> get props => [counter];
+
+  @override
+  String toString() => 'CounterState(counter: $counter)';
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+}
